4ea2b9e17c833ac4fda9c8b9cabdc5f06da147bb
Ashwini3243 <ashwinirakam3243@gmail.com>
2020-08-28 18:20:14 +0530

    first commit

--- src/main/java/nendrasys/controller/HomeController.java
+++ src/main/java/nendrasys/controller/HomeController.java
@@ -1,13 +1,16 @@
 package nendrasys.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("home")
-    public String home(){
+    public String home(Model model)
+    {
+        model.addAttribute("msg","practice");
         return "homePage";
     }
 }
