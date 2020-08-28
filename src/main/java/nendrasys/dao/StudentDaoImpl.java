acbdc55d23351cde6b3d9cda62838cd133c45f2d
sauravkr827 <sauravkr82711@gmail.com>
2020-08-28 17:46:39 +0530

    no message

--- src/main/java/nendrasys/dao/StudentDaoImpl.java
+++ src/main/java/nendrasys/dao/StudentDaoImpl.java
@@ -15,7 +15,12 @@ public class StudentDaoImpl implements StudDao
       s1.setId(1);
       s1.setName("saurav");
       s1.setAge(23);
-        studentArryList.add(s1);
+
+        Student s2= new Student();
+        s2.setId(2);
+        s2.setName("saurav kumar");
+        s2.setAge(23);
+        studentArryList.add(s2);
        return studentArryList;
     }
 
