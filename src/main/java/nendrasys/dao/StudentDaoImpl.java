01a0a3c452128ecb4e2a748f6589610bfb6dac16
Ashwini3243 <ashwinirakam3243@gmail.com>
2020-08-31 11:47:44 +0530

    ha

--- src/main/java/nendrasys/dao/StudentDaoImpl.java
+++ src/main/java/nendrasys/dao/StudentDaoImpl.java
@@ -10,18 +10,17 @@ public class StudentDaoImpl implements StudDao
     @Override
     public List<Student> getStudentDetails()
     {
+        
         ArrayList<Student> studentArryList = new ArrayList<Student>();
         Student s1= new Student();
-      s1.setId(1);
-      s1.setName("saurav");
+        s1.setId(1);
         s1.setName("saurav");
         s1.setName("saurav");
         s1.setName("saurav");
         s1.setName("saurav");
-
-      s1.setAge(23);
-s1.setId(1);
-
+        s1.setName("saurav");
+        s1.setAge(23);
+        s1.setId(1);
 
         studentArryList.add(s1);
        return studentArryList;
